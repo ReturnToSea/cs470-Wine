8a000f7fea4662644d1e5251e234c28c870db773
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-10-29 11:47:39 -0700

    changed to work with all data

--- Data/SplitData.py
+++ Data/SplitData.py
@@ -3,17 +3,27 @@
 import pandas as pd
 import os
 
-file_path = 'Sales Registers/Till Export 021222.xls'
-
-df = pd.read_excel(file_path)
+input_folder = 'Sales Registers'
 
 output1 = 'register1'
 output2 = 'register2'
 
-first = df.iloc[:, :9] #A-I
-second = df.iloc[:, 9:] #Everything after
+counter = 1
 
-#change output name when changing to work on all files
-first.to_csv(os.path.join(output1, 'r1_1.csv'), index=False)
-second.to_csv(os.path.join(output2, 'r2_2.csv'), index=False)
+for file_name in os.listdir(input_folder):
+    if file_name.endswith('.xls') or file_name.endswith('xlsx'):
+        file_path = os.path.join(input_folder, file_name)
+
+        df = pd.read_excel(file_path)
+
+        first_path = df.iloc[:, :9] #A-I
+        second_path = df.iloc[:, 9:] #Everything after
+
+        first_output = os.path.join(output1, f'r1_{counter}.csv')
+        second_output = os.path.join(output2, f'r2_{counter}.csv')
+
+        first_path.to_csv(first_output, index=False)
+        second_path.to_csv(second_output, index=False)
+
+        counter += 1
 
