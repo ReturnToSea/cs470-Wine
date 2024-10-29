7f37caddfe02ea428795bdeb53122e5d5ffc1e69
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-10-29 15:33:36 -0700

    changed csv reader

--- Data/ReadCsv.py
+++ Data/ReadCsv.py
@@ -7,18 +7,6 @@ with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
     csv_reader = csv.reader(csvfile)
     read_data = False
     for row in csv_reader:
-        try:
-            date = pd.to_datetime(row[0], errors='raise')
-            if not read_data:
-                read_data = True
-                print(date)
-        except ValueError:
-            pass
-        if read_data:
-            #change this to work with all payment types
-            #also add here for if it reads in a new date before a payment type
-            #it will reset since that last data is incomplete
-            if 'EFTPOS' in row:
-                #the next non empty value will be the cost
-                break
-            print(row)
+        for cell in row:
+            if cell.strip():
+                print(cell)
