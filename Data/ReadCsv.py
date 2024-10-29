ed09c02b04718933a9b171422887e7f27b4a2589
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-10-29 11:20:56 -0700

    updated csv reader to read in a date and all rows until payment

--- Data/ReadCsv.py
+++ Data/ReadCsv.py
@@ -1,9 +1,23 @@
 import csv
+import pandas as pd
 
 file_path = 'Register1/r1_1.csv'
 
 with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
     csv_reader = csv.reader(csvfile)
-    
+    read_data = False
     for row in csv_reader:
-        print(row)
+        try:
+            date = pd.to_datetime(row[0], errors='raise')
+            if not read_data:
+                read_data = True
+                print(date)
+        except ValueError:
+            pass
+        if read_data:
+            #change this to work with all payment types
+            #also add here for if it reads in a new date before a payment type
+            #it will reset since that last data is incomplete
+            if 'EFTPOS' in row: 
+                break
+            print(row)
