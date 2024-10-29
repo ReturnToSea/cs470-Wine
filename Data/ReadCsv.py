ef07fc49c9804d24b55c7cdb6b81f19a83ab1d6d
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-10-29 16:16:18 -0700

    changed csv file

--- Data/ReadCsv.py
+++ Data/ReadCsv.py
@@ -1,7 +1,13 @@
 import csv
 import pandas as pd
+import re
 
 file_path = 'Register1/r1_1.csv'
+
+date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$')
+read_data = False
+
+payment_types = ("EFTPOS", "CASH", "AMEX")
 
 with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -9,4 +15,8 @@ with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
     for row in csv_reader:
         for cell in row:
             if cell.strip():
-                print(cell)
+                if date_pattern.match(cell.strip()): #Find Day (can treat this as start of new order)
+                    read_data = True
+                    print(cell)
+                if read_data:
+                    print(cell)
