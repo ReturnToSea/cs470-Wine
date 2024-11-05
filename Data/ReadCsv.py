9a6dcaaa1dc384718e98599458152e787844b602
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-11-05 05:01:51 -0800

    changed csv reader changed what dictionary is using

--- Data/ReadCsv.py
+++ Data/ReadCsv.py
@@ -2,21 +2,22 @@ import csv
 import pandas as pd
 import re
 
-file_path = 'Register1/r1_1.csv'
+file_path = 'Register1/r1_2.csv'
+output_file = 'CsvReaderOutput.txt'
 
 date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$')
 read_data = False
 
 payment_types = ("EFTPOS", "CASH", "AMEX")
-
-with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    read_data = False
-    for row in csv_reader:
-        for cell in row:
-            if cell.strip():
-                if date_pattern.match(cell.strip()): #Find Day (can treat this as start of new order)
-                    read_data = True
-                    print(cell)
-                if read_data:
-                    print(cell)
+with open(output_file, mode='w', newline='', encoding='utf-8') as output_file_obj:
+    with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        read_data = False
+        for row in csv_reader:
+            for cell in row:
+                if cell.strip():
+                    if date_pattern.match(cell.strip()): #Find Day (can treat this as start of new order)
+                        read_data = True
+                        #output_file_obj.write(cell + '\n')
+                    if read_data:
+                        output_file_obj.write(cell + '\n')
