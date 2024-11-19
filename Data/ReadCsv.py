23bdefca8642c42439ec1ebb5d8c7b881726d8de
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-11-19 15:09:40 -0800

    Changed to work with register 2 as well

--- Data/ReadCsv.py
+++ Data/ReadCsv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 import os
 
-directory_path = 'Register1'
+directories = ['Register1', 'Register2']
 output_file = 'CsvReaderOutput.txt'
 
 date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$')
@@ -11,17 +11,18 @@ read_data = False
 
 payment_types = ("EFTPOS", "CASH", "AMEX")
 with open(output_file, mode='w', newline='', encoding='utf-8') as output_file_obj:
-    for filename in os.listdir(directory_path):
-        if filename.endswith('.csv'):
-            file_path = os.path.join(directory_path, filename)
-            with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
-                csv_reader = csv.reader(csvfile)
-                read_data = False
-                for row in csv_reader:
-                    for cell in row:
-                        if cell.strip():
-                            if date_pattern.match(cell.strip()): #Find Day (can treat this as start of new order)
-                                read_data = True
-                                #output_file_obj.write(cell + '\n')
-                            if read_data:
-                                output_file_obj.write(cell + '\n')
+    for directory_path in directories:
+        for filename in os.listdir(directory_path):
+            if filename.endswith('.csv') or (directory_path == 'Register2' and filename.startswith('r2_')):
+                file_path = os.path.join(directory_path, filename)
+                with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
+                    csv_reader = csv.reader(csvfile)
+                    read_data = False
+                    for row in csv_reader:
+                        for cell in row:
+                            if cell.strip():
+                                if date_pattern.match(cell.strip()): #Find Day (can treat this as start of new order)
+                                    read_data = True
+                                    #output_file_obj.write(cell + '\n')
+                                if read_data:
+                                    output_file_obj.write(cell + '\n')
